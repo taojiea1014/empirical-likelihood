209b2222995a711310df06a6989dab6d1870de28
wuxiaoqiang1993 <wuxq19931223@gmail.com>
2017-03-17 15:54:01 -0400

    Update find_confidence_inteval.m

--- find_confidence_inteval.m
+++ find_confidence_inteval.m
@@ -17,6 +17,7 @@ function [y1, y2]=find_confidence_inteval(x,method, level_alpha, res,iter)
     u_candi = x1;
     result=[];
     while (u_candi <= x_end)
+        %Newton's method with iteration 1000, initial value 0.11
         lamda = find_lamda(0.11, 1000,x,u_candi);
         if two_logT(lamda,x, u_candi)<threshold
             result = [result, u_candi];
