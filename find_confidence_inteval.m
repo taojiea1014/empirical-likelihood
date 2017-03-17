0d948c5e8d50053b0e36f3aa4582270fd3e00d74
wuxiaoqiang1993 <wuxq19931223@gmail.com>
2017-03-17 15:44:10 -0400

    Update find_confidence_inteval.m

--- find_confidence_inteval.m
+++ find_confidence_inteval.m
@@ -1,3 +1,10 @@
+%   x is data, which must be scalar, horizontal vector.
+%   method is the method used to determine threshold according to difference distribution
+%   'boostrap' is using boostrap 2log(T) distribution
+%   'chisq' is using approximately chisquare distribution with degree freedom of 1.
+%   level_alpha is the alpha used in 'chisqure'
+%   res is the grid search resolution
+%   iter is the number of iteration in 'boostrap method'
 function [y1, y2]=find_confidence_inteval(x,method, level_alpha, res,iter) 
     if strcmpi(method,'boostrap')
         threshold = get_boostrap_threshold(iter,x,level_alpha);
