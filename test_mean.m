c5ce226b1e0bcd628bdbe6f8f4787756584767d5
wuxiaoqiang1993 <wuxq19931223@gmail.com>
2017-04-03 11:12:36 -0400

    Update test_mean.m

--- test_mean.m
+++ test_mean.m
@@ -1,4 +1,4 @@
-%   return the test statistics 2log(T) given data and u
+%   return the test statistics 2log(T) given data and u, x is the data in a horizontal vector, u0 is the mean value we want to test
 function y = test_mean(x,u0)
     %get lamda using newton method
     lamda = find_lamda(0.11, 20,x,u0);
